25a59c9a31640c2edac2a38fc910a7e69a085484
levi9111 <captainlevi9111@gmail.com>
2025-07-17 22:52:44 +0600

    Map screen initiated

---
+++ app/(tabs)/map.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+// For showing the map on the screen
+
+const Map = () => {
+  return (
+    <View>
+      <Text>Map</Text>
+    </View>
+  );
+};
+
+export default Map;
